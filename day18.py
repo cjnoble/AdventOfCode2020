7fc3c77c4195cdfd76c40977114a882fef4d1a25
Chris Noble <NobleChrisJ@gmail.com>
2023-12-11 21:22:39 +0000

    Better late than never

--- day18.py
+++ day18.py
@@ -13,7 +13,7 @@ def prep_data(data):
     return [[c for c in line if c != " "] for line in data]
 
 def calc_p (calc_string):
-    #print(f"Calcing {calc_string}")
+    print(f"Calcing {calc_string}")
 
     operations_dict = {
 
@@ -37,8 +37,6 @@ def calc_p (calc_string):
 
 def calc_p1_ordering(calc_string):
     print(f"Eval: {calc_string}")
-    #open_brackt = [a=="(" for a in calc_string]
-    #close_bracket = [a==")" for a in calc_string]
 
     cur_string = []
 
@@ -46,7 +44,7 @@ def calc_p1_ordering(calc_string):
 
         var = calc_string.pop(0)
         if var == "(":
-            calc_string = (calc_p1_ordering(calc_string))
+            calc_string = calc_p1_ordering(calc_string)
             #print(f"After applying brackets {cur_string}")
 
         elif var in ["+", "-", "*" ,"/"]:
@@ -65,6 +63,35 @@ def calc_p1_ordering(calc_string):
     return [calc_p(cur_string)]
 
 
+def calc_p2_eval_line(calc_string):
+    print(f"Eval Line: {calc_string}")
+    cur_string = []
+
+    while len(calc_string) > 0:
+        print(cur_string, calc_string)
+
+        var = calc_string.pop(0)
+        if var == "*" or var == "/":
+            if len(cur_string)>0:
+                cur_string = [calc_p(cur_string)] # Do addition first
+            cur_string.append(var)
+            cur_string.extend(calc_p2_eval_line(calc_string)) # Now eval the rest of the line
+            print(f"New cur string {cur_string}")
+       
+        elif var == "+" or var == "-":
+            cur_string.append(var)
+
+        elif var == "(" or var == ")":
+            raise Exception("Unexpected Bracket")
+
+        else:
+            cur_string.append(var)
+
+    result = [calc_p(cur_string)]
+    print(f"Eval Line Result: {result}")
+    return result
+
+
 def calc_p2_ordering(calc_string):
     print(f"Eval: {calc_string}")
 
@@ -75,28 +102,20 @@ def calc_p2_ordering(calc_string):
 
         var = calc_string.pop(0)
         if var == "(":
-            cur_string.extend(calc_p2_ordering(calc_string))
-            calc_string = calc_p2_ordering(cur_string)
-
-        elif var == "*" or var == "/":
-            cur_string.append(var)
-            calc_string  = calc_p2_ordering(calc_string)
-            #cur_string = calc_p(cur_string)
-            
-        elif var == "+" or var == "-":
-            cur_string = [calc_p(cur_string)]
-            cur_string.append(var)
-            #return cal_val
+            cur_string.extend(calc_p2_ordering(calc_string)) #look inside the brackets and extend onto what we already have
+            print(f"New cur string for ordering {cur_string}")
+            return calc_p2_ordering(cur_string) #re evaluate the whole thing
 
         elif var == ")":
-            cal_val = [calc_p(cur_string)]
-            cal_val.extend(calc_string)
-            return cal_val
+            cal_val = calc_p2_eval_line(cur_string) #evaluate brackets contents
+            if calc_string:
+                cal_val.extend(calc_string) # extend on what was after the brackets
+            return cal_val # return to op brackets
 
         else:
             cur_string.append(var)
 
-    return [calc_p(cur_string)]
+    return calc_p2_eval_line(cur_string)
 
 
 def part_1_verbose(data):
--- day18_test.py
+++ day18_test.py
@@ -18,17 +18,17 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(day18.calc_p1_ordering(day18.prep_data(["5 + 3 * 2 * (9 + 6 * 6 + 9) * 3"])[0])[0], 4752)
 
     def test_calc_part2(self):
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["1 + 2"])[0])[0], 3)
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["1 + 2 * 3 + 4 * 5 + 6"])[0])[0], 231)
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["1 + (2 * 3) + (4 * (5 + 6))"])[0])[0], 51)
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["2 * 3 + (4 * 5)"])[0])[0], 46)
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["5 + (8 * 3 + 9 + 3 * 4 * 3)"])[0])[0],  1445)
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"])[0])[0], 669060)
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["6 + 9 * 8 + 6"])[0])[0], 210)
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6)"])[0])[0], (210+6)*54)
-        #self.assertEqual(day18.calc_p2_ordering([11664, "+",  "2",  "+",  "4",  "*", "2"])[0], 23340)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["1 + 2"])[0])[0], 3)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["1 + 2 * 3 + 4 * 5 + 6"])[0])[0], 231)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["1 + (2 * 3) + (4 * (5 + 6))"])[0])[0], 51)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["2 * 3 + (4 * 5)"])[0])[0], 46)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["5 + (8 * 3 + 9 + 3 * 4 * 3)"])[0])[0],  1445)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"])[0])[0], 669060)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["6 + 9 * 8 + 6"])[0])[0], 210)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6)"])[0])[0], (210+6)*54)
+        self.assertEqual(day18.calc_p2_ordering([11664, "+",  "2",  "+",  "4",  "*", "2"])[0], 23340)
         self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"])[0])[0], 23340)
-        #self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["((9 + 6 * 4 * 4 * 7 * 5) + 3 * 5 * 7 * 2) * 9 + 9 * (3 * (8 * 2 * 9 * 2 * 5 + 8) + (7 * 2 * 7) * 2)"])[0])[0], 23340)
+        self.assertEqual(day18.calc_p2_ordering(day18.prep_data(["((9 + 6 * 4 * 4 * 7 * 5) + 3 * 5 * 7 * 2) * 9 + 9 * (3 * (8 * 2 * 9 * 2 * 5 + 8) + (7 * 2 * 7) * 2)"])[0])[0], 244069504560)
 
 if __name__ == '__main__':
     unittest.main()
